efc903e97d7e5524806135cb1f0fa6093e66b4a2
jawadaf <jafa0001@stud.kea.dk>
2024-10-07 18:21:15 +0200

    I have created package 'js' contains the general http named 'moduleJson.js' and a movie.js with movie.html and movie.css.

---
+++ js/moduleJson.js
@@ -0,0 +1,69 @@
+async function postObjectAsJson(url, object, httpVerbum) {
+    const objectAsJsonString = JSON.stringify(object)
+    console.log(objectAsJsonString)
+    const fetchOptions = {
+        method: httpVerbum,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: objectAsJsonString
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+
+async function createMethod(url, object) {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: object ? JSON.stringify(object) : null
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+
+async function getAllMethod(url) {
+    const fetchOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+
+async function updateMethod(url, object) {
+    const fetchOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: object ? JSON.stringify(object) : null
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+
+async function deleteMethod(url) {
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+function fetchAnyUrl(url) {
+    return fetch(url).then(response => response.json())
+}
+
+export {postObjectAsJson, createMethod, getAllMethod, updateMethod, deleteMethod, fetchAnyUrl}
